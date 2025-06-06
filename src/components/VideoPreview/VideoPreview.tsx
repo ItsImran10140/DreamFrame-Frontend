5ca56e73712277a6765e56c3d4c236a29a0048ff
Imran Shah <imranshah10140@gmail.com>
2025-06-06 22:11:30 +0530

    Failed to load Project Data Fixed the UI

--- src/components/VideoPreview/VideoPreview.tsx
+++ src/components/VideoPreview/VideoPreview.tsx
@@ -12,18 +12,15 @@ const VideoPreview = ({
   return (
     <div className="bg-zinc-800/40 rounded-lg border-[0.75px] border-zinc-800/40 mb-2 pb-2 md:mb-3 flex-shrink-0 overflow-hidden mt-2">
       {loading ? (
-        <div className="flex items-center justify-center h-full">
+        <div className="flex items-center justify-center h-[50vh]">
           <div className="flex flex-col items-center">
-            <div className="animate-spin rounded-full h-6 w-6 md:h-8 md:w-8 border-t-2 border-b-2 border-green-500 mb-2 md:mb-4"></div>
+            <div className="animate-spin rounded-full h-6 w-6 md:h-8 md:w-8 border-t-2 border-b-2 border-zinc-500 mb-2 md:mb-4"></div>
             <p className="text-gray-400 text-sm">Loading video editor...</p>
           </div>
         </div>
       ) : error ? (
-        <div className="flex items-center justify-center h-full">
+        <div className="flex items-center justify-center h-[50vh]">
           <div className="text-red-400 flex flex-col items-center">
-            <span className="text-red-500 text-3xl md:text-5xl mb-2 md:mb-4">
-              !
-            </span>
             <p className="text-sm text-center px-4">{error}</p>
           </div>
         </div>
